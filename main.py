6d5c8aba8520e03ad61a3ba5ffe580635b57ea6f
Miks Matīss Liepiņš <126872246+221RDB275@users.noreply.github.com>
2023-03-19 21:44:15 +0000

    versija1

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ def parallel_processing(n, m, data):
     output = []
     # TODO: write the function for simulating parallel tasks, 
     # create the output pairs
-
+    brivais = [0] * n
+    for i in range(m):
+        nakamais = 0
+        laiks = brivais[nakamais]
+        output.append((nakamais, laiks))
+        brivais[nakamais] = brivais[nakamais] + data[i]
     return output
 
 def main():
@@ -13,18 +18,20 @@ def main():
     # first line - n and m
     # n - thread count 
     # m - job count
-    n = 0
-    m = 0
+            #n = 0
+            #m = 0
+    n, m = map(int, input().split())
 
     # second line - data 
     # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
-    data = []
+    data = list(map(int, input().split()))
 
     # TODO: create the function
     result = parallel_processing(n,m,data)
     
     # TODO: print out the results, each pair in it's own line
-
+    for paris in result:
+        print(paris[0], paris[1])
 
 
 if __name__ == "__main__":
